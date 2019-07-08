4f495548bc4ad26c95d225f2b0308c6ffd9410f0
José Carlos S.A. Tissei <jose.tissei@hbsis.com.br>
2019-07-08 20:01:33 -0300

    v3

---
+++ campo-obrigatorio/university-v3/Migrations/20190708213344_Person-Email-Fill-Empty.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using ContosoUniversity.Data;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContosoUniversity.Migrations
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// The person email fill empty.
+    /// </summary>
+    public partial class PersonEmailFillEmpty : Migration
+    {
+        private const int AMOUNT_PER_ITERATION = 100;
+        private const string PLACEHOLDER = "empty";
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            using (var context = new DesignTimeSchoolContext().CreateDbContext(new string[] { }))
+            {
+                var total = context.People.Count();
+                var iterations = Math.Ceiling((decimal)total / AMOUNT_PER_ITERATION);
+
+                for (var i = 0; i < iterations; i++)
+                {
+                    var students = context
+                        .People
+                        .OrderBy(x => x.ID)
+                        .Skip(i * AMOUNT_PER_ITERATION)
+                        .Take(AMOUNT_PER_ITERATION)
+                        .Where(x => x.Email == null || x.Email == string.Empty)
+                        .ToList();
+
+                    students.ForEach(x => x.Email = PLACEHOLDER);
+                    context.People.UpdateRange(students);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            using (var context = new DesignTimeSchoolContext().CreateDbContext(new string[] { }))
+            {
+                var total = context.People.Count();
+                var iterations = Math.Ceiling((decimal)total / AMOUNT_PER_ITERATION);
+
+                for (var i = 0; i < iterations; i++)
+                {
+                    var students = context
+                        .People
+                        .OrderBy(x => x.ID)
+                        .Skip(i * AMOUNT_PER_ITERATION)
+                        .Take(AMOUNT_PER_ITERATION)
+                        .Where(x => x.Email == PLACEHOLDER)
+                        .ToList();
+
+                    students.ForEach(x => x.Email = null);
+                    context.People.UpdateRange(students);
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
---
+++ campo-obrigatorio/university-v3/Migrations/20190708215003_Person-Email-Required.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContosoUniversity.Migrations
+{
+    public partial class PersonEmailRequired : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Person",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Person",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+        }
+    }
+}
