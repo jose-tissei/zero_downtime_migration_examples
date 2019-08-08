b432d02245689a8a0a3c43696edfb71f79368a2f
José Carlos S.A. Tissei <jose.tissei@hbsis.com.br>
2019-08-08 10:59:52 -0300

    carga de instrutores e v3 finalizada

--- campo-obrigatorio/university/Migrations/20190328124431_Instructor-Seed.Designer.cs
+++ campo-obrigatorio/university/Migrations/20190328124431_Instructor-Seed.Designer.cs
@@ -4,14 +4,16 @@ using ContosoUniversity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ContosoUniversity.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20190328124431_Instructor-Seed")]
+    partial class InstructorSeed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
